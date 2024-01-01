7a4ca4003f01d9c466b2c6b1491fa53ca52d9972
Aleks <feniksizvdv@ukr.net>
2024-01-01 19:25:20 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    static double convertingMilesToKilometer (double miles){
+        double km = miles/0.62137;
+        return km;
+    }
+}
