9f7776f99366c07b5dcbd7dcf17ec19e3ddb47fe
Aleks <feniksizvdv@ukr.net>
2024-01-01 19:46:45 +0200

    add example for kilometers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.format("%.3f", convertingMilesToKilometer(3));
-
+        convertingKilometersToMiles(3);
     }
     static double convertingMilesToKilometer (double miles){
         double km = miles/0.62137;
